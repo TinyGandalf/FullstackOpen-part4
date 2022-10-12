9580c00744a858289697dd5a221a566019437181
Giuliano Rosso <rossogiuliano@gmail.com>
2022-10-12 19:56:03 -0300

    Refactored tests

--- bloglist/tests/blogs_api.test.js
+++ bloglist/tests/blogs_api.test.js
@@ -45,129 +45,129 @@ const initialBlogs = [
 
 beforeEach(async () => {
   await Blog.deleteMany({})
-
-  for (const blog of initialBlogs) {
-    const blogObject = new Blog(blog)
-    await blogObject.save()
-  }
+  await Blog.insertMany(initialBlogs)
 })
 
-test('blogs are returned as JSON', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
+describe('when some blogs are already present', () => { 
+  test('blogs are returned as JSON', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
 
-test('the right amount of blogs are returned', async () => {
-  const response = await api.get('/api/blogs')
+  test('the right amount of blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
 
-  expect(response.body).toHaveLength(initialBlogs.length)
-})
+    expect(response.body).toHaveLength(initialBlogs.length)
+  })
 
-test('blogs are returned as JSON', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
+  test('blogs are returned as JSON', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
 
-test('the right amount of blogs are returned', async () => {
-  const response = await api.get('/api/blogs')
+  test('the right amount of blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
 
-  expect(response.body).toHaveLength(initialBlogs.length)
-})
+    expect(response.body).toHaveLength(initialBlogs.length)
+  })
 
-test('the blogs returned contain their id as "id"', async () => {
-  const response = await api.get('/api/blogs')
+  test('the blogs returned contain their id as "id"', async () => {
+    const response = await api.get('/api/blogs')
 
-  for (const blog of response.body) {
-    expect(blog.id).toBeDefined()
-  }
-})
+    for (const blog of response.body) {
+      expect(blog.id).toBeDefined()
+    }
+  })
 
-test('a valid blog can be added ', async () => {
-  const newBlog = {
-    title: "React anti-patterns",
-    author: "Cichael Mhan",
-    url: "https://angularjs.com/",
-    likes: 70
-  }
+  test('all blogs should have a likes field', async () => {
+    const response = await api.get('/api/blogs')
+    const blogsAtEnd = response.body
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+    const likeFields = blogsAtEnd.map(n => n.likes)
+    expect(likeFields).toBeDefined()
+    expect(likeFields).not.toBeNaN()
+  })
 
-  const response = await api.get('/api/blogs')
-  const blogsAtEnd = response.body
-
-  expect(blogsAtEnd).toHaveLength(initialBlogs.length + 1)
-  const titles = blogsAtEnd.map(n => n.title)
-  expect(titles).toContain(
-    'React anti-patterns'
-  )
-})
-
-test('all blogs should have a likes field', async () => {
-  const response = await api.get('/api/blogs')
-  const blogsAtEnd = response.body
-
-  const likeFields = blogsAtEnd.map(n => n.likes)
-  expect(likeFields).toBeDefined()
-  expect(likeFields).not.toBeNaN()
-})
-
-test('if the likes field is ommited it defaults to 0', async () => {
-  const newBlog = {
-    title: "React anti-patterns",
-    author: "Cichael Mhan",
-    url: "https://angularjs.com/"
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
-  const response = await api.get('/api/blogs')
-  const blogsAtEnd = response.body
-
-  expect(blogsAtEnd).toHaveLength(initialBlogs.length + 1)
-
-  const blog = blogsAtEnd.filter(blog => blog.title === newBlog.title)
-  expect(blog[0]).toBeDefined()
-  expect(blog[0].likes).toEqual(0)
-})
-
-test('if the title field is ommited the api returns a 400 error', async () => {
-  const newBlog = {
-    //title: "React anti-patterns",
-    author: "Cichael Mhan",
-    url: "https://angularjs.com/",
-    likes: 0
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
-})
-
-test('if the url field is ommited the api returns a 400 error', async () => {
-  const newBlog = {
-    title: "React anti-patterns",
-    author: "Cichael Mhan",
-    //url: "https://angularjs.com/",
-    likes: 0
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
+  describe('adding blogs', () => { 
+    test('a valid blog can be added ', async () => {
+      const newBlog = {
+        title: "React anti-patterns",
+        author: "Cichael Mhan",
+        url: "https://angularjs.com/",
+        likes: 70
+      }
+  
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+  
+      const response = await api.get('/api/blogs')
+      const blogsAtEnd = response.body
+  
+      expect(blogsAtEnd).toHaveLength(initialBlogs.length + 1)
+      const titles = blogsAtEnd.map(n => n.title)
+      expect(titles).toContain(
+        'React anti-patterns'
+      )
+    })
+  
+    test('if the likes field is ommited it defaults to 0', async () => {
+      const newBlog = {
+        title: "React anti-patterns",
+        author: "Cichael Mhan",
+        url: "https://angularjs.com/"
+      }
+  
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+  
+      const response = await api.get('/api/blogs')
+      const blogsAtEnd = response.body
+  
+      expect(blogsAtEnd).toHaveLength(initialBlogs.length + 1)
+  
+      const blog = blogsAtEnd.filter(blog => blog.title === newBlog.title)
+      expect(blog[0]).toBeDefined()
+      expect(blog[0].likes).toEqual(0)
+    })
+  
+    test('if the title field is ommited the api returns a 400 error', async () => {
+      const newBlog = {
+        //title: "React anti-patterns",
+        author: "Cichael Mhan",
+        url: "https://angularjs.com/",
+        likes: 0
+      }
+  
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400)
+    })
+  
+    test('if the url field is ommited the api returns a 400 error', async () => {
+      const newBlog = {
+        title: "React anti-patterns",
+        author: "Cichael Mhan",
+        //url: "https://angularjs.com/",
+        likes: 0
+      }
+  
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400)
+    })
+  })
 })
 
 afterAll(async () => {
