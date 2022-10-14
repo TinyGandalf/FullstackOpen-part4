c83ddee184eafb430bd9b500f208cdd95a56ecca
Giuliano Rosso <rossogiuliano@gmail.com>
2022-10-14 17:08:25 -0300

    Refactored tests and moved commonly used methods into test_helpers

--- bloglist/tests/blogs_api.test.js
+++ bloglist/tests/blogs_api.test.js
@@ -3,45 +3,7 @@ const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
 const Blog = require('../models/blog')
-
-const initialBlogs = [
-  {
-    title: "React patterns",
-    author: "Michael Chan",
-    url: "https://reactpatterns.com/",
-    likes: 7,
-  },
-  {
-    title: "Go To Statement Considered Harmful",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-    likes: 5,
-  },
-  {
-    title: "Canonical string reduction",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-    likes: 12,
-  },
-  {
-    title: "First class tests",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-    likes: 10,
-  },
-  {
-    title: "TDD harms architecture",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-    likes: 0,
-  },
-  {
-    title: "Type wars",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-    likes: 2
-  }
-]
+const { initialBlogs, nonExistingBlogId } = require('./test_helpers')
 
 beforeEach(async () => {
   await Blog.deleteMany({})
@@ -192,11 +154,7 @@ describe('when some blogs are already present', () => {
       let response = await api.get('/api/blogs')
       const blogsBefore = response.body
 
-      let id = '63474a7f4a3a2ca8186626f7'
-      while (blogsBefore.find(blog => blog.id === id)) {
-        randomNumber = Math.floor(Math.random() * 10)
-        id = `${randomNumber}474a7f4a3a2ca8186626f7`
-      }
+      let id = await nonExistingBlogId()
 
       await api
         .delete(`/api/blogs/${id}`)
@@ -261,14 +219,7 @@ describe('when some blogs are already present', () => {
     })
 
     test('updating an non existing blog returns code 404', async () => { 
-      let response = await api.get('/api/blogs')
-      const blogsBefore = response.body
-
-      let id = '63474a7f4a3a2ca8186626f7'
-      while (blogsBefore.find(blog => blog.id === id)) {
-        randomNumber = Math.floor(Math.random() * 10)
-        id = `${randomNumber}474a7f4a3a2ca8186626f7`
-      }
+      let id = await nonExistingBlogId()
 
       await api
         .put(`/api/blogs/${id}`)
