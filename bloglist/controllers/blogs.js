0312d9ca31a844a5531d647a84080751008e6799
Giuliano Rosso <rossogiuliano@gmail.com>
2022-10-17 17:06:12 -0300

    (Exercise 4.17) Added user id to blogs

--- bloglist/controllers/blogs.js
+++ bloglist/controllers/blogs.js
@@ -1,8 +1,11 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+    .find({})
+    .populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
@@ -11,9 +14,12 @@ blogsRouter.post('/', async (request, response) => {
     return response.status(400).end()
   }
 
+  const user = await User.findOne({})
+
   const blog = new Blog({
     ...request.body,
-    likes: request.body.likes ?? 0
+    likes: request.body.likes ?? 0,
+    user: user.id
   })
   const result = await blog.save()
 
